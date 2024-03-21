9237aba1c4d7432644f3766ca194d5be5aae9c90
dinozindev <pugplayzcraft@gmail.com>
2024-03-20 21:17:12 -0300

    cp atualizado

--- cp-1/cp.py
+++ cp-1/cp.py
@@ -2,10 +2,21 @@
 
 valorVenda = input("Venda........................: ")
 valorVenda = float(valorVenda)
-cupom = input(f"Tem cupom, [s]im ou [n]ão?..: ")
 
+# verifica valores negativos
+# while valorVenda < 0:
+#     valorVenda = input("Venda........................: ")
+#     valorVenda = float(valorVenda)
+
+# verifica se o valor digitado é 's' ou 'n'. Se não, perguntar novamente
+cupom = input(f"Tem cupom, [s]im ou [n]ão?..: ")
+while cupom != "s" and cupom != "n":
+   cupom = input(f"Tem cupom, [s]im ou [n]ão?..: ") 
+
+# 1% do valor da venda
 umPorcento = valorVenda / 100
 
+# verificação e aplicação do desconto
 if valorVenda <= 1000:
     desconto = umPorcento * 5
     valorDesconto = valorVenda - desconto
@@ -13,11 +24,16 @@ elif valorVenda > 1000:
     desconto = umPorcento * 10
     valorDesconto = valorVenda - desconto
 
+# verificação e aplicação do desconto do cupom
 if cupom == "s":
     cupomValor = 50
     valorDesconto = valorDesconto - 50
 elif cupom == "n": 
     cupomValor = 0
+
+# evitar valores negativos após desconto
+# if valorDesconto < 0:
+#     valorDesconto = 0
 
 print("RELATÓRIO: ")
 print(f"Venda........: {valorVenda:.2f}")
