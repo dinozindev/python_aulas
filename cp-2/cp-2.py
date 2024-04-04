fd3feeba8bc7e8c4a72c543ed4824eb500e00bf8
dinozindev <pugplayzcraft@gmail.com>
2024-04-04 15:24:06 -0300

    finalizando cp2

--- cp-2/cp-2.py
+++ cp-2/cp-2.py
@@ -2,10 +2,11 @@
 # CP2 – Prof. MSc. Luiz W Tavares – 03 Abril 2024
 # RM554424 – Nome: Lucas Kenji Kikuchi
 
-# oq falta: finalizar votação quando todos os eleitores ja tiverem votado, imprimir relatorio
-
+# lista de eleitores
 eleitores = ['111', '222', '333', '444', '555']
+# lista se votou (1) ou não (0)
 votouOuNao = [0, 0, 0, 0, 0]
+# lista vazia de candidatos
 candidatos = []
 # ultimo voto é nulo (len(votosCandidatos) - 1)
 votosCandidatos = [0]
@@ -13,36 +14,38 @@ votosCandidatos = [0]
 
 # cadastro dos candidatos
 while True:
-    candidatoAtual = input("Número do Candidato ou <fim>:  ")
+    candidatoAtual = input("\nNúmero do Candidato ou <fim>:  ")
+    # se digitar fim, quebra o while
     if candidatoAtual.lower() == 'fim':
-        print("\n")
         break
     # somente admite valores entre 10 e 99
     if int(candidatoAtual) >= 10 and int(candidatoAtual) <= 99:
         candidatos.append(candidatoAtual)
         votosCandidatos.append(0)
-        print(f"Nome do Candidato...........:  Candidato {candidatoAtual} \n")
-        print(candidatos) #tirar
+        print(f"Nome do Candidato...........:  Candidato {candidatoAtual}")
 
 # votação    
 while True:
-    eleitorAtual = input("Digite o Título do Eleitor ou <fim> para terminar:  ")
+    eleitorAtual = input("\nDigite o Título do Eleitor ou <fim> para terminar:  ")
+    # se digitar fim, quebra o while
     if eleitorAtual.lower() == 'fim':
-        print("\n")
         break
+    # verifica se o eleitor está presente na lista de eleitores
     if eleitores.count(eleitorAtual) == 0:
-        print("\nEleitor não autorizado para votar\n")
+        print("\nEleitor não autorizado para votar")
+    
     elif eleitores.count(eleitorAtual) == 1:
         # verifica se o eleitor já votou
         if votouOuNao[eleitores.index(eleitorAtual)] == 1:
-            print("\nEleitor já votou!!!\n")
+            print("\nEleitor já votou!!!")
         else:
             print("\nInício do Voto: \n")
-            # percorre o array de candidatos, imprimindo cada um
+            print("Candidatos:")
+            # percorre a lista de candidatos, imprimindo cada um
             for i in range(len(candidatos)):
                 print(f"{candidatos[i]} - Candidato {candidatos[i]}")
             voto = input("\nVoto - Número do Candidato:  ")
-            # contabiliza os votos
+            # adiciona o voto ao candidato correspondente
             for i in range(len(candidatos)):
                 if candidatos[i] == voto:
                     votosCandidatos[i]+=1
@@ -51,7 +54,24 @@ while True:
                     votosCandidatos[len(votosCandidatos) - 1]+=1
                     break
             # atualiza a lista votouOuNao, indicando se o eleitor votou (1) ou não (0)
-            votouOuNao[eleitores.index(eleitorAtual)]+=1 
+            votouOuNao[eleitores.index(eleitorAtual)]+=1
+    # verificação para caso todos os eleitores já tiverem votado 
+    if votouOuNao.count(1) == 5:
+        print(f"\nTodos os eleitores já votaram.\nFIM\n")
+        break
 
-print(votouOuNao) #tirar
-print(votosCandidatos) #tirar
+# relatório
+print("=========================================")
+print(f"Total de eleitores: {len(eleitores)}")
+print(f"Total de votos ...: {votouOuNao.count(1)}")
+print(f"\nCandidatos ........:")
+print(f"Votos:{(votosCandidatos[len(votosCandidatos) - 1]):>3} - 000 - Votos Nulo")
+for i in range(len(votosCandidatos) - 1):
+        print(f"Votos:{votosCandidatos[i]:>3} -  {candidatos[i]} - Candidato {candidatos[i]}")
+print("\nEleitor:")
+for i in range(len(eleitores)):
+    if votouOuNao[i] == 0:
+        print(f"{eleitores[i]} - NÃO votou")
+    else:
+        print(f"{eleitores[i]} - votou")
+
