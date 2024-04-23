3d0b7bbcd80f8dd61f805c3fdd08d72c25c6f82a
Lucas <124621971+dinozindev@users.noreply.github.com>
2024-04-23 11:17:02 -0300

    Exercicio 4

--- exercicios0422/teste.py
+++ exercicios0422/teste.py
@@ -1,11 +1,17 @@
 # Exercício 4 - Contando caracteres 
 dicionario = {} 
 frase = input("Escreva uma frase: ")
+# remove os espaços em branco da frase
 fraseSemEspacos = frase.replace(' ', '')
+# transforma a string em uma lista
 caracteresFrase = list(fraseSemEspacos)
-print(caracteresFrase.index('o'))
 
-for i in range(len(caracteresFrase)):
-    dicionario[caracteresFrase[i]] = 1
+# para cada caractere na lista, se ela já estiver na lista, adiciona um em sua contagem. Caso contrário, adiciona ele na lista.
+for caractere in caracteresFrase:
+    if caractere in dicionario:
+        dicionario[caractere] += 1
+    else:
+        dicionario[caractere] = 1
 
 print(dicionario)
+
