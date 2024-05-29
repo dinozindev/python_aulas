7c9d2e0f263044a3808079b6cab80731b5b16069
dinozin <dinozindev@gmail.com>
2024-05-29 16:52:25 -0300

    adicionando funcionalidade de denunciar poluição

--- gs/gs.py
+++ gs/gs.py
@@ -1,11 +1,32 @@
 import re;
 
+organizacoes = [{'nome': 'Oceanos Mais Limpos', 'pontuacao': 1200}, {'nome': 'Mares Azuis', 'pontuacao': 1000}, {'nome': 'WaterEco', 'pontuacao': 800}, {'nome': 'OceansBy', 'pontuacao': 600}, {'nome': 'SustainableMarine', 'pontuacao': 400}]
+
+tipos_poluicao = ['Plástico', 'Vazamento de óleo', 'Detritos de embarcações', 'Descarte de esgoto', 'Produtos químicos']
+oceanos = ['Oceano Índico', 'Oceano Antártico', 'Oceano Pacífico', 'Oceano Atlântico', 'Oceano Ártico']
+qtnd = ['Baixa', 'Média', 'Alta', 'Alarmante']
+
+#lista de especies
+especies_marinhas = [
+    {'nome': 'Tartaruga-verde','nome_cientifico': 'Chelonia mydas','descricao': 'A tartaruga-verde é uma grande tartaruga marinha encontrada principalmente em mares tropicais e subtropicais. É herbívora e se alimenta principalmente de algas e ervas marinhas.','status_conservacao': 'Em Perigo'}, 
+    {'nome': 'Baleia-azul','nome_cientifico': 'Balaenoptera musculus','descricao': 'A baleia-azul é o maior animal conhecido a ter existido, podendo atingir mais de 30 metros de comprimento. Alimenta-se principalmente de krill e outras pequenas criaturas marinhas.','status_conservacao': 'Em Perigo'}, 
+    {'nome': 'Golfinho-nariz-de-garrafa','nome_cientifico': 'Tursiops truncatus','descricao': 'O golfinho-nariz-de-garrafa é uma espécie de golfinho amplamente distribuída em mares temperados e tropicais. É conhecido por sua inteligência e comportamento sociável.','status_conservacao': 'Pouco Preocupante'},
+    {'nome': 'Tubarão-branco','nome_cientifico': 'Carcharodon carcharias','descricao': 'O tubarão-branco é um dos maiores predadores marinhos, conhecido por seu tamanho e força. Habita águas costeiras temperadas e subtropicais em todo o mundo.','status_conservacao': 'Vulnerável'},
+    {'nome': 'Peixe-palhaço','nome_cientifico': 'Amphiprioninae','descricao': 'O peixe-palhaço é conhecido por sua relação simbiótica com anêmonas-do-mar. Possui cores vibrantes e é popular em aquários marinhos.','status_conservacao': 'Pouco Preocupante'},
+    {'nome': 'Polvo-gigante-do-pacífico','nome_cientifico': 'Enteroctopus dofleini','descricao': 'O polvo-gigante-do-pacífico é a maior espécie de polvo, encontrada no Oceano Pacífico Norte. É conhecido por sua inteligência e capacidade de camuflagem.','status_conservacao': 'Pouco Preocupante'},
+    {'nome': 'Manati','nome_cientifico': 'Trichechus','descricao': 'O manati, também conhecido como peixe-boi, é um grande mamífero marinho herbívoro encontrado em águas costeiras e rios de regiões tropicais e subtropicais.','status_conservacao': 'Vulnerável'},
+    {'nome': 'Cavalo-marinho','nome_cientifico': 'Hippocampus','descricao': 'O cavalo-marinho é um pequeno peixe marinho com uma aparência única que lembra um cavalo. É conhecido por suas caudas preênseis e a reprodução onde os machos carregam os ovos.','status_conservacao': 'Vulnerável'},
+    {'nome': 'Estrela-do-mar girassol','nome_cientifico': 'Pycnopodia helianthoides','descricao': 'A estrela-do-mar girassol é uma das maiores espécies de estrela-do-mar, encontrada no Pacífico Norte. Tem muitas pernas e é um predador eficiente.','status_conservacao': 'Em Perigo Crítico'},
+    {'nome': 'Raia-manta','nome_cientifico': 'Manta birostris','descricao': 'A raia-manta é uma das maiores espécies de raia, conhecida por sua grande envergadura e comportamento grácil. Habita águas tropicais e subtropicais.','status_conservacao': 'Vulnerável'}
+]
+
 usuarioDados = []
+denunciaPesca = []
+denunciaPoluicao = []
 regexCpf = r'^\d{3}\.\d{3}\.\d{3}-\d{2}$'
 regexTel = r'\d{2} 9\d{4}-\d{4}'
 regexNome = r"^[A-Za-zÀ-ÿ'\- ]+$"
-
-#lista de especies
+regexCoordenadas = r'^([-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)),\s*([-+]?(180(\.0+)?|(1[0-7]\d|[1-9]?\d)(\.\d+)?))$'
 
 # cadastro do usuario
 def cadastro_usuario():
@@ -25,7 +46,7 @@ def cadastro_usuario():
     # cadastro idade   
     while True:
         idade = input("Digite sua idade.........................: ")
-        if not idade.isdigit() or int(idade) < 18 or int(idade) > 90:
+        if not idade.isdigit() or int(idade) < 13 or int(idade) > 90:
             print("Digite uma idade válida.")
             continue
         else:
@@ -110,18 +131,133 @@ def gerenciar_usuario(usuarioDados):
                 print("Retornando ao menu do usuário...")
                 continue
 
-def relatar_pesca_irregular():
-   print("\nRelatório de pesca irregular...")
-     
+def denunciar_pesca_ilegal():
+    print("\nIniciando denúncia de pesca ilegal...")
+    if usuarioDados == []:
+        print("Você não está cadastrado. Irei te redirecionar para o menu...")
+        return
+    while True:
+        coordenadas = input("Digite as coordenadas (Ex: -45.12345, -123.12345): ")
+        if re.match(regexCoordenadas, coordenadas) is None:
+            print("Digite um nome válido.")
+            continue
+        else:
+            denunciaPoluicao.append(coordenadas)
+            break
+    while True:
+        print("\n==============[ OCEANO ]==============\n")
+        for i in range(len(oceanos)):
+            print(f"{i} - {oceanos[i]}")
+        opt_oceano = input("\nEm qual oceano a poluição foi encontrada?: ")
+        if not opt_oceano.isdigit() or (int(opt_oceano) > 4) or int(opt_oceano) < 0:
+            print("\nSelecione uma opção válida.")
+            continue
+        else:
+            opt_oceano = int(opt_oceano)
+            denunciaPoluicao.append(oceanos[opt_oceano])
+            break
+    while True:
+        print("\n==============[ QUANTIDADE ]==============\n")
+        for i in range(len(qtnd)):
+            print(f"{i} - {qtnd[i]}")
+        opt_qtnd = input("\nQual o nível da quantidade encontrada?: ")
+        if not opt_qtnd.isdigit() or (int(opt_qtnd) > 3) or int(opt_qtnd) < 0:
+            print("\nSelecione uma opção válida.")
+            continue
+        else:
+            opt_qtnd = int(opt_qtnd)
+            denunciaPoluicao.append(qtnd[opt_qtnd])
+            break
+    while True:
+        coordenadas = input("Digite as coordenadas (Ex: -45.12345, -123.12345): ")
+        if re.match(regexCoordenadas, coordenadas) is None:
+            print("Digite um nome válido.")
+            continue
+        else:
+            denunciaPoluicao.append(coordenadas)
+            break
 
+def denunciar_poluicao():
+    print("\nIniciando denúncia de poluição...\n")
+    if usuarioDados == []:
+        print("Você não está cadastrado. Irei te redirecionar para o menu...")
+        return
+    while True: 
+        print("\n==============[ TIPO DE POLUIÇÃO ]==============\n")
+        for i in range(len(tipos_poluicao)):
+            print(f"{i} - {tipos_poluicao[i]}")
+        opt_poluicao = input("\nQual o tipo de poluição encontrada?: ")
+        if not opt_poluicao.isdigit() or (int(opt_poluicao) > 4) or int(opt_poluicao) < 0:
+            print("\nSelecione uma opção válida.")
+            continue
+        else:
+            opt_poluicao = int(opt_poluicao)
+            denunciaPoluicao.append(tipos_poluicao[opt_poluicao])
+            break
+    while True:
+        print("\n==============[ OCEANO ]==============\n")
+        for i in range(len(oceanos)):
+            print(f"{i} - {oceanos[i]}")
+        opt_oceano = input("\nEm qual oceano a poluição foi encontrada?: ")
+        if not opt_oceano.isdigit() or (int(opt_oceano) > 4) or int(opt_oceano) < 0:
+            print("\nSelecione uma opção válida.")
+            continue
+        else:
+            opt_oceano = int(opt_oceano)
+            denunciaPoluicao.append(oceanos[opt_oceano])
+            break
+    while True:
+        print("\n==============[ QUANTIDADE ]==============\n")
+        for i in range(len(qtnd)):
+            print(f"{i} - {qtnd[i]}")
+        opt_qtnd = input("\nQual o nível da quantidade encontrada?: ")
+        if not opt_qtnd.isdigit() or (int(opt_qtnd) > 3) or int(opt_qtnd) < 0:
+            print("\nSelecione uma opção válida.")
+            continue
+        else:
+            opt_qtnd = int(opt_qtnd)
+            denunciaPoluicao.append(qtnd[opt_qtnd])
+            break
+    while True:
+        coordenadas = input("Digite as coordenadas (Ex: -45.12345, -123.12345): ")
+        if re.match(regexCoordenadas, coordenadas) is None:
+            print("Digite um nome válido.")
+            continue
+        else:
+            denunciaPoluicao.append(coordenadas)
+            break
+    ponto_referencia = input('Qual o ponto de referência?: ')
+    denunciaPoluicao.append(ponto_referencia)
+    print("\nDenúncia de poluição registrada com sucesso.")
+
+# pontuação das organizações    
+def pontuacao_organizacoes():
+    posicao = 1
+    print("\n==============[ TABELA DE ORGANIZAÇÕES ]==============\n") 
+    organizacoes_sorted = sorted(organizacoes, key=lambda x: x['pontuacao'], reverse=True)
+    for organizacao in organizacoes_sorted:
+        print(f"{posicao}ª - {organizacao['nome']}, Pontuação: {organizacao['pontuacao']}")
+        posicao+=1
+    input("\nPressione ENTER para voltar ao menu principal: ")
+
+# consulta especies
+def consultar_especies():
+
+
+
+# menu    
 while True:
     print("\n==============[ MENU ]==============\n")
     print("1  - Cadastro Usuário")
     print("2  - Gerenciar Usuário")
-    print("3  - Relatar pesca irregular")
+    print("3  - Denunciar Pesca Ilegal")
+    print("4  - Denunciar Poluição")
+    print("5  - Relatar Espécie")
+    print("6  - Consultar Espécies")
+    print("7  - Visualizar pontuação das organizações")
     print("0  - Sair\n")
     option = input("Opção: ")
-    if not option.isdigit() or (int(option) > 10 or int(option) < 0):
+    if not option.isdigit() or (int(option) > 7 or int(option) < 0):
         print("\nSelecione uma opção válida.")
         continue
     option = int(option)   
@@ -134,4 +270,12 @@ while True:
         case 2:
             gerenciar_usuario(usuarioDados)
         case 3:
-            relatar_pesca_irregular()
+            denunciar_pesca_ilegal()
+        case 4:
+            denunciar_poluicao()
+        case 5:
+            print()
+        case 6:
+            consultar_especies()
+        case 7:
+            pontuacao_organizacoes()
