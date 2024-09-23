148b8bf909370ac83feb4394c31d1555ebb91af3
dinozin <dinozindev@gmail.com>
2024-09-23 14:29:04 -0300

    exemplos

--- 0909/0909.py
+++ 0909/0909.py
@@ -7,3 +7,37 @@ with open('exemplo.txt', 'r') as arquivo:
 with open('exemplo.txt', 'r') as arquivo:
     conteudo = arquivo.read()
     print(conteudo)
+
+with open('exemplo.txt', 'w') as arquivo:
+    arquivo.write("Novo conteúdo")
+
+with open('arquivo.txt', 'a') as arquivo:
+    arquivo.write("Adicionando conteúdo")
+
+with open('arquivo.txt', 'x') as arquivo:
+    arquivo.write("Criando novo arquivo")
+
+# binarios
+with open('imagem.png', 'rb') as arquivo:
+    dados = arquivo.read()
+
+with open('imagem.png', 'wb') as arquivo:
+    arquivo.write(novos_dados_binarios)
+
+with open('imagem.png', 'ab') as arquivo:
+    arquivo.write(mais_dados_binarios)
+
+# atualizacao
+with open('arquivo.txt', 'r+') as arquivo:
+    conteudo = arquivo.read()
+    arquivo.write("Novo conteúdo no final")
+
+with open('arquivo.txt', 'w+') as arquivo:
+    arquivo.write("Novo conteúdo")
+    arquivo.seek(0)
+    conteudo = arquivo.read()
+
+with open('arquivo.txt', 'a+') as arquivo:
+    arquivo.write("Conteúdo adicional")
+    arquivo.seek(0)
+    conteudo = arquivo.read()
